67cf811d88451e8927e59678e59396884daddf0a
Théo Brouillé <theo.brouille@kering.com>
2024-10-29 01:13:43 +0100

    feat(bubble): improve drag gesture (wip)

--- Sources/PulseUI/Views/BubbleView.swift
+++ Sources/PulseUI/Views/BubbleView.swift
@@ -12,8 +12,9 @@ import Pulse
 @available(iOS 14, *)
 public struct BubbleView: View {
     @State private var isConsoleViewPresented: Bool = false
-    @State private var position: CGPoint = CGPoint(x: 0, y: 50)
-    @State private var bubbleSize: CGSize = CGSize()
+    @State private var position: CGPoint = .zero
+    @State private var safeAreaSize: CGSize = .zero
+    @State private var bubbleSize: CGSize = .zero
 
     private let store: LoggerStore
     private let consoleMode: ConsoleMode
@@ -24,6 +25,31 @@ public struct BubbleView: View {
     }
 
     public var body: some View {
+        ZStack {
+            Color.clear
+                .onSizeChange { size in
+                    print("🔍 \(size)")
+                    safeAreaSize = size
+                }
+
+            bubbleButton
+                .onSizeChange { size in
+                    bubbleSize = size
+                }
+//                .position(position)
+                .offset(x: position.x - bubbleSize.width / 2, y: position.y - bubbleSize.height / 2)
+                .highPriorityGesture(dragGesture)
+                .fullScreenCover(isPresented: $isConsoleViewPresented) {
+                    NavigationView {
+                        ConsoleView(store: store, mode: consoleMode)
+                    }
+                }
+        }
+    }
+}
+
+private extension BubbleView {
+    var bubbleButton: some View {
         Button {
             isConsoleViewPresented = true
         } label: {
@@ -33,23 +59,39 @@ public struct BubbleView: View {
                 .padding(4)
                 .background(Circle().fill(.blue))
         }
-        .saveSize(in: $bubbleSize)
-        .offset(x: position.x - bubbleSize.width / 2, y: position.y - bubbleSize.height / 2)
-        .highPriorityGesture(dragGesture)
-        .fullScreenCover(isPresented: $isConsoleViewPresented) {
-            NavigationView {
-                ConsoleView(store: store, mode: consoleMode)
-            }
-        }
     }
-}
 
-private extension BubbleView {
     var dragGesture: some Gesture {
         DragGesture()
             .onChanged { gesture in
                 withAnimation {
                     position = gesture.location
+                }
+            }
+            .onEnded { gesture in
+                let leftBorder = -safeAreaSize.width / 2
+                let rightBorder = safeAreaSize.width / 2
+                let topBorder = -safeAreaSize.height / 2
+                let bottomBorder = safeAreaSize.height / 2
+
+                let padding: CGFloat = 4
+
+                let x: CGFloat = if gesture.location.x < 0 {
+                    leftBorder + bubbleSize.width + padding
+                } else {
+                    rightBorder - padding
+                }
+
+                let y: CGFloat = if gesture.location.y - bubbleSize.height - padding < topBorder {
+                    topBorder + bubbleSize.height + padding
+                } else if gesture.location.y > bottomBorder {
+                    bottomBorder - padding
+                } else {
+                    gesture.location.y - padding
+                }
+
+                withAnimation {
+                    position = CGPoint(x: x, y: y)
                 }
             }
     }
